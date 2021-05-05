d0e540f2740978b407c111604a8b6e777d77c7c6
montecamo <merrychristek@gmail.com>
2021-05-05 11:04:47 +0300

    make whitelist accept regexp

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 80;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = process.env.CORSANYWHERE_WHITELIST ? new RegExp(process.env.CORSANYWHERE_WHITELIST) : '';
 function parseEnvList(env) {
   if (!env) {
     return [];
